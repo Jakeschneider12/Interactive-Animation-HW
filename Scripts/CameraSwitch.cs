c305dca4abc6bc39477f93b60a5c968ccc9cd877
Jakeschneider12 <jakeschneider12@gmail.com>
2016-04-07 06:01:32 -0500

    Original File

---
+++ Scripts/CameraSwitch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+    public Camera Camera1;
+    public Camera Camera2;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Camera1.enabled = false;
+            Camera2.enabled = true;
+        }
+    }
+
+}
---
+++ Scripts/Respawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour {
+
+    void OnTriggerEnter (Collider other)
+    {
+      ;
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
